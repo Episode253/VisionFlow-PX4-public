b7154f2aa0baf1833f71073daf825218424c3944
Renwang Huang <RenwangHuangX@gmail.com>
2026-05-31 00:10:52 +0800

    Fix firmware compilation issues (Batch 2)

--- src/lib/version/version.c
+++ src/lib/version/version.c
@@ -1,36 +1,3 @@
-/****************************************************************************
- *
- *   Copyright (c) 2016 PX4 Development Team. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name PX4 nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- ****************************************************************************/
-
 #include "version.h"
 
 #include "build_git_version.h"
@@ -44,6 +11,39 @@
 #if defined(__PX4_LINUX)
 #include <sys/utsname.h>
 #endif
+
+#ifndef PX4_GIT_VERSION_STR
+#define PX4_GIT_VERSION_STR "visionflow-px4"
+#endif
+
+#ifndef PX4_GIT_TAG_STR
+#define PX4_GIT_TAG_STR "v1.17.0"
+#endif
+
+#ifndef PX4_GIT_BRANCH_NAME
+#define PX4_GIT_BRANCH_NAME "visionflow"
+#endif
+
+#ifndef PX4_GIT_VERSION_BINARY
+#define PX4_GIT_VERSION_BINARY 0ULL
+#endif
+
+#ifndef PX4_GIT_OEM_VERSION_STR
+#define PX4_GIT_OEM_VERSION_STR ""
+#endif
+
+#ifndef NUTTX_GIT_VERSION_STR
+#define NUTTX_GIT_VERSION_STR "visionflow-nuttx"
+#endif
+
+#ifndef NUTTX_GIT_TAG_STR
+#define NUTTX_GIT_TAG_STR "v1.0.0"
+#endif
+
+#ifndef NUTTX_GIT_VERSION_BINARY
+#define NUTTX_GIT_VERSION_BINARY 0ULL
+#endif
+
 
 // dev >= 0
 // alpha >= 64
