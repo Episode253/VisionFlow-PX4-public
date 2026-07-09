d1357c02ffe570926e8024a05fbdd4b5c05f2430
Renwang Huang <RenwangHuangX@gmail.com>
2026-07-09 18:05:16 +0800

    Remove landing gear logic.

--- src/modules/pregme_att_control_v2/pregme_att_control.hpp
+++ src/modules/pregme_att_control_v2/pregme_att_control.hpp
@@ -25,7 +25,6 @@
 #include <lib/mathlib/math/filter/AlphaFilter.hpp>
 
 #include <uORB/topics/battery_status.h>
-#include <uORB/topics/landing_gear.h>
 #include <uORB/topics/rate_ctrl_status.h>
 #include <uORB/topics/vehicle_angular_velocity.h>
 #include <uORB/topics/vehicle_torque_setpoint.h>
@@ -66,7 +65,6 @@ private:
 
 	void update_vehicle_status();
 	void update_landed_state();
-	void update_landing_gear();
 	void publish_torque_thrust_setpoint(const vehicle_attitude_s &v_att);
 
 	Att_Control _attitude_control;
@@ -78,57 +76,60 @@ private:
 	uORB::Subscription _manual_control_setpoint_sub{ORB_ID(manual_control_setpoint)};
 	uORB::Subscription _vehicle_status_sub{ORB_ID(vehicle_status)};
 	uORB::Subscription _vehicle_land_detected_sub{ORB_ID(vehicle_land_detected)};
+	uORB::Subscription _battery_status_sub{ORB_ID(battery_status)};
+
 	uORB::SubscriptionCallbackWorkItem _vehicle_attitude_sub{this, ORB_ID(vehicle_attitude)};
 	uORB::SubscriptionCallbackWorkItem _local_pos_sub{this, ORB_ID(vehicle_local_position)};
-
-	uORB::Publication<vehicle_attitude_setpoint_s> _vehicle_attitude_setpoint_pub{ORB_ID(vehicle_attitude_setpoint)};
-
-	uORB::Subscription _battery_status_sub{ORB_ID(battery_status)};
-	uORB::Subscription _landing_gear_sub{ORB_ID(landing_gear)};
 	uORB::SubscriptionCallbackWorkItem _vehicle_angular_velocity_sub{this, ORB_ID(vehicle_angular_velocity)};
+
 	uORB::PublicationMulti<rate_ctrl_status_s> _controller_status_pub{ORB_ID(rate_ctrl_status)};
 
 	uORB::Publication<vehicle_torque_setpoint_s> _vehicle_torque_setpoint_pub{ORB_ID(vehicle_torque_setpoint)};
 	uORB::Publication<vehicle_thrust_setpoint_s> _vehicle_thrust_setpoint_pub{ORB_ID(vehicle_thrust_setpoint)};
+	uORB::Publication<vehicle_attitude_setpoint_s> _vehicle_attitude_setpoint_pub{ORB_ID(vehicle_attitude_setpoint)};
 
-	bool _actuators_0_circuit_breaker_enabled{false};
-
-	vehicle_control_mode_s _vehicle_control_mode{};
+        vehicle_control_mode_s _vehicle_control_mode{};
 	vehicle_status_s _vehicle_status{};
-
-	float _battery_status_scale{0.0f};
-
-	perf_counter_t _loop_perf{nullptr};
-
-	float _thrust_sp{0.0f};
 
 	hrt_abstime _last_run{0};
 	hrt_abstime _last_attitude_setpoint{0};
 
-	int8_t _landing_gear{landing_gear_s::GEAR_DOWN};
+	perf_counter_t _loop_perf{nullptr};
+
+	bool _actuators_0_circuit_breaker_enabled{false};
+
+	float _battery_status_scale{0.0f};
+
+	float _thrust_sp{0.0f};
 
 	manual_control_setpoint_s _manual_control_setpoint{};
 
-	matrix::Vector3f _thrust_setpoint_body{0.f, 0.f, 0.f};
+	float _man_tilt_max{0.f};
 
 	float _man_yaw_sp{0.f};
-	float _man_tilt_max{0.f};
+	uint8_t _quat_reset_counter{0};
+
+	// Yaw 从当前机头方向开始，主要在 Manul Stablize 模式参与的情况下使用
+	bool _reset_yaw_sp{true};
+
 	AlphaFilter<float> _man_x_input_filter;
 	AlphaFilter<float> _man_y_input_filter;
 
 	bool _landed{true};
 	bool _maybe_landed{true};
-	bool _reset_yaw_sp{true};
+	bool _spooled_up{false};
+
+	// 默认四旋翼无人机，禁用VTOL逻辑
 	bool _vehicle_type_rotary_wing{true};
 	bool _vtol_in_transition_mode{false};
 	bool _vtol_tailsitter{false};
-	bool _spooled_up{false};
 
-	uint8_t _quat_reset_counter{0};
+	matrix::Vector3f _thrust_setpoint_body{0.f, 0.f, 0.f};
 
+	// 表示机体坐标系下的 Roll、Pitch、Yaw 三个轴上的控制力矩
 	matrix::Vector3f _torque{0.f, 0.f, 0.f};
-	float pos_z{0.f};
 
+	float pos_z{0.f};
 
 	DEFINE_PARAMETERS(
 		(ParamFloat<px4::params::USR_TAU_COE>) _param_usr_tau_coe,
@@ -139,7 +140,6 @@ private:
 		(ParamFloat<px4::params::USR_I_XY>) _param_usr_i_xy,
 		(ParamFloat<px4::params::USR_I_XZ>) _param_usr_i_xz,
 		(ParamFloat<px4::params::USR_I_YZ>) _param_usr_i_yz,
-
 
 		(ParamFloat<px4::params::USR_LAMBDA_Q_X>) _param_usr_lambda_q_x,
 		(ParamFloat<px4::params::USR_LAMBDA_Q_Y>) _param_usr_lambda_q_y,
@@ -164,13 +164,13 @@ private:
 		(ParamFloat<px4::params::USR_ROLLRATE_MAX>) _param_usr_rollrate_max,
 		(ParamFloat<px4::params::USR_PITRATE_MAX>) _param_usr_pitchrate_max,
 		(ParamFloat<px4::params::USR_YAWRATE_MAX>) _param_usr_yawrate_max,
-
 		(ParamFloat<px4::params::USR_MAN_Y_MAX>) _param_usr_man_y_max,
 
 		(ParamInt<px4::params::USR_AIRMODE>) _param_usr_airmode,
 		(ParamFloat<px4::params::USR_MAN_TILT_TAU>) _param_usr_man_tilt_tau,
 		(ParamFloat<px4::params::USR_MAN_TILT_MAX>) _param_usr_man_tilt_max,
 		(ParamFloat<px4::params::USR_MANTHR_MIN>) _param_usr_manthr_min,
+
 		(ParamFloat<px4::params::USR_THR_MAX>) _param_usr_thr_max,
 		(ParamFloat<px4::params::USR_THR_HOVER>) _param_usr_thr_hover,
 		(ParamInt<px4::params::USR_THR_CURVE>) _param_usr_thr_curve,
