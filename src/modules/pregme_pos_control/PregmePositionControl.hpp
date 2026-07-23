45d6fba44dd51013498d7edaa6726ce6b816b282
Renwang Huang <RenwangHuangX@gmail.com>
2026-07-23 18:17:32 +0800

    Fix the issue that the parameter name of the attitude loop center-of-mass compensation switch is too long.

--- src/modules/pregme_pos_control/PregmePositionControl.hpp
+++ src/modules/pregme_pos_control/PregmePositionControl.hpp
@@ -143,7 +143,7 @@ private:
 		(ParamFloat<px4::params::PREGME_XYVALL>) _param_pregme_xy_vel_all,
 		(ParamFloat<px4::params::PREGME_ZVALL>) _param_pregme_z_vel_all,
 		(ParamFloat<px4::params::PREGME_VELD_LP>) _param_pregme_veld_lp,
-		(ParamBool<px4::params::PREGME_COM_COMP_EN>) _param_pregme_com_comp_en
+		(ParamBool<px4::params::PREGME_COMCP_EN>) _param_pregme_com_comp_en
 	);
 
 	struct VelocityDerivativeFilter {
